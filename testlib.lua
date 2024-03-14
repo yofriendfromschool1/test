c98f819fcda12527fe64624d26562883c4313e6a
Sky <153390719+yofriendfromschool1@users.noreply.github.com>
2024-03-13 22:32:54 -0700

    Update testlib.lua

--- testlib.lua
+++ testlib.lua
@@ -60,37 +60,7 @@ local TempTab = Instance.new("ScrollingFrame")
 local UIGridLayout_3 = Instance.new("UIGridLayout")
 local Script = Instance.new("Script", ImageLabel)
 Script.Name = "Script"
-UserInputService = game:GetService("UserInputService")
-local IsOnMobile = table.find({
-	Enum.Platform.IOS,
-	Enum.Platform.Android
-}, UserInputService:GetPlatform())
 
-if IsOnMobile then
-	local Sky = {}
-	Sky["Ui"] = Instance.new("ScreenGui", gethui())
-	Sky["Ui"].Name = "skyhubtoggle"
-
-	Sky["DaIcon"] = Instance.new("ImageButton", Sky["Ui"])
-	Sky["DaIcon"].Size = UDim2.new(0,45,0,45)
-	Sky["DaIcon"].Position = UDim2.new(.001,0,0.5,0)
-	Sky["DaIcon"].Draggable = true
-	Sky["DaIcon"].Image = "rbxassetid://12010069146"
-	Sky["DaIcon"].BackgroundColor3 = Color3.fromRGB(17, 36, 66)
-	Sky["DaIcon"].MouseButton1Click:Connect(function()
-		script.Parent.Visible = false
-		for i,v in pairs(game:GetService("CoreGui"):GetChildren()) do
-			if v.Name == "skyhub" then
-				v.Frame.Visible = true
-			end
-		end
-	end)
-	Sky["das"] = Instance.new("UICorner", Sky["DaIcon"]);
-	Sky["das"]["CornerRadius"] = UDim.new(0.20000000298023224, 0);
-	Sky["DaIcon"].Visible = false
-else
-
-end
 
 --Properties:
 if game:WaitForChild("CoreGui") then
@@ -102,7 +72,6 @@ if game:WaitForChild("CoreGui") then
 else
 	ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
 end
-ScreenGui.Name = "skyhub"
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 ImageLabel.Parent = ScreenGui
@@ -789,12 +758,7 @@ local function HLBC_fake_script() -- ImageLabel.Script
 	
 	runService.Heartbeat:Connect(Update)
 	script.Parent.TopBar.Minimize.Activated:Connect(function()
-		if IsOnMobile then
-			Sky["DaIcon"].Visible = true
-    		visible = false
-		else
-			visible = false
-		end
+		visible = false
 	end)
 	script.Parent.TopBar.Close.Activated:Connect(function()
 		close = true
